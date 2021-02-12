53b6e4d24feb9202dfc186905503b957f2024873
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-02-12 20:48:03 -0300

    fix savinggoaltitle style and tests

--- src/app/components/SavingGoalTitle/styles.ts
+++ src/app/components/SavingGoalTitle/styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   display: flex;
   place-items: center;
+  margin-bottom: 1.5rem;
 `
 export const HomeIcon = styled.img`
   margin-right: 1.6rem;
