83ee7b6fd611dd3dba93fcd6b4104ce260b82e16
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-02-12 00:52:38 -0300

    adding reducer types

--- src/app/models/SavingGoal/index.ts
+++ src/app/models/SavingGoal/index.ts
@@ -1,3 +1,14 @@
 export interface MinDate {
   initialDate: Date
 }
+
+export interface SavingGoalState {
+  reachDate: Date
+  totalAmount: string
+  monthlyAmount: string
+}
+
+export type Actions =
+  | { type: 'SET_TOTAL_AMOUNT'; money: string }
+  | { type: 'SET_DATE'; date: Date }
+  | { type: 'SET_MONTHLY_AMOUNT'; amount: string }
