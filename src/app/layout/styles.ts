7fe040559fae3f4a435d4235365356dce52bd89f
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-02-12 02:58:25 -0300

    fix styles

--- src/app/layout/styles.ts
+++ src/app/layout/styles.ts
@@ -8,16 +8,18 @@ const customMedia = generateMedia({
 
 export const Content = styled.div`
   padding: 3.2rem 2.4rem 4rem;
+  max-width: 60.9rem;
+  background: #ffffff;
   ${customMedia.greaterThan('desktop')`
     padding: 3.5rem 4rem 4rem;
     border-radius: 0.8rem;
   `}
 `
 export const Main = styled.main`
-  background: #ffffff;
-
   ${customMedia.greaterThan('medium')`
     margin-left: 5rem;
     margin-right: 5rem;
+    display: grid;
+    place-items: center;
   `}
 `
