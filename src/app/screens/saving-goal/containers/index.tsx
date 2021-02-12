6d37afcda4e4bce996cbceed9acc24eb14783114
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-02-11 22:19:09 -0300

    exporting SavingGoalContainer

---
+++ src/app/screens/saving-goal/containers/index.tsx
@@ -0,0 +1,15 @@
+import DatePickerContainer from './DatePickerContainer'
+import MoneyInputContainer from './MoneyInputContainer'
+import SavingPlanContainer from './SavingPlanContainer'
+
+const SavingGoalContainer = () => {
+  return (
+    <>
+      <MoneyInputContainer />
+      <DatePickerContainer />
+      <SavingPlanContainer />
+    </>
+  )
+}
+
+export default SavingGoalContainer
