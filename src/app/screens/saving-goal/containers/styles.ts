9d6fe8f60b1fee2cbfb70f556b521b2d547d40e3
luiznascimento_ <luiz.nascimento@br.experian.com>
2021-02-13 01:49:26 -0300

    fix savingoal media query

--- src/app/screens/saving-goal/containers/styles.ts
+++ src/app/screens/saving-goal/containers/styles.ts
@@ -2,11 +2,12 @@ import styled from 'styled-components'
 import { generateMedia } from 'styled-media-query'
 
 const customMedia = generateMedia({
+  medium: '560px',
   desktop: '690px'
 })
 
 export const InputWrapper = styled.div`
-  ${customMedia.greaterThan('desktop')`
+  ${customMedia.greaterThan('medium')`
     display: flex;
     justify-content: space-between;
   `}
